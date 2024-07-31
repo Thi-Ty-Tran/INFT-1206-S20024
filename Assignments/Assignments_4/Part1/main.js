b7d146c2ae877418dd52cca658206fafaede5684
Thi-Ty-Tran <ty.tran@dcmail.ca>
2024-07-31 14:43:46 -0400

    update main.js

--- Assignments/Assignments_4/Part1/main.js
+++ Assignments/Assignments_4/Part1/main.js
@@ -37,20 +37,20 @@ function result() {
   const zItem = randomValueFromArray(insertZ);
 
   newStory = newStory.replaceAll(":insertx:", xItem)
-        .replace(":inserty:", yItem)
-        .replace(":insertz:", zItem);
+    .replace(":inserty:", yItem)
+    .replace(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replaceAll('Bob', name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+    newStory = newStory.replace("94 fahrenheit", temperature).replace("300 pounds", weight);
+}
 
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+story.textContent = newStory;
+story.style.visibility = 'visible';
 }
