fb739e4610e81c3bd7fa963709c5ece624cf25a3
Thi-Ty-Tran <ty.tran@dcmail.ca>
2024-08-05 03:54:54 -0400

    loop through images - assignment 4 part 2

--- Assignments/Assignments_4/Part2/main.js
+++ Assignments/Assignments_4/Part2/main.js
@@ -11,6 +11,16 @@ const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 const alt = ["pic 1", "pic 2", "pic 3", "pic 4", "pic 5"];
 
 /* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alt[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', a => {
+        displayedImage.src = a.target.src;
+        displayedImage.alt = a.target.alt;
+    })
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
