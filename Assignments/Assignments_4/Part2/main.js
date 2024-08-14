d93397cb94bf290333e31707dd53490b7d88a99e
Thi-Ty-Tran <ty.tran@dcmail.ca>
2024-08-14 14:06:40 -0400

    update main.js part 2-ass4

--- Assignments/Assignments_4/Part2/main.js
+++ Assignments/Assignments_4/Part2/main.js
@@ -6,11 +6,10 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
-
 /* Declaring the alternative text for each image file */
 const alt = ["pic 1", "pic 2", "pic 3", "pic 4", "pic 5"];
-
 /* Looping through images */
+
 for (const image of images) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${image}`);
@@ -21,11 +20,6 @@ for (const image of images) {
         displayedImage.alt = a.target.alt;
     })
 }
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', e => {
